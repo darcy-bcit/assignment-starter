2dba607277c4b440c268183ed76d054851626317
evin-gg <evin4369@gmail.com>
2025-02-10 19:09:32 -0800

    Added code that generates docx skeletons for assignment hand-ins

--- cli/main.go
+++ cli/main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
-    "fmt"
-    
-    "github.com/darcy-bcit/assignment-starter/cli/internal/codegen"
-    "github.com/darcy-bcit/assignment-starter/cli/internal/docs"
-    "github.com/darcy-bcit/assignment-starter/cli/internal/fsm"
-    "github.com/darcy-bcit/assignment-starter/cli/internal/yml"
+	"log"
+
+	"github.com/gomutex/godocx"
 )
 
 func main() {
-    fmt.Println("Hello, World")
+	// Open an existing DOCX document
+	// document, err := godocx.OpenDocument("./testdata/test.docx")
+
+	// Create New Document
+	document, err := godocx.NewDocument()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Save the modified document to a new file
+	err = document.SaveTo("./output/report/user-guide.docx")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = document.SaveTo("./output/report/testing.docx")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = document.SaveTo("./output/report/report.docx")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = document.SaveTo("./output/report/design.docx")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
