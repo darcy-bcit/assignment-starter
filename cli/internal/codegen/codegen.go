48696815fc96be1f92d196519aa4cd51a82fb9fd
kvnbanunu <kvnbanunu@gmail.com>
2025-02-10 23:00:45 -0800

    Function for generation directory

--- cli/internal/codegen/codegen.go
+++ cli/internal/codegen/codegen.go
@@ -1,1 +1,33 @@
 package codegen
+
+import (
+    "fmt"
+    "log"
+    "os"
+)
+
+func GenCode() {
+}
+
+func genDir(path string) {
+    err := os.MkdirAll(path, 0755)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func genFile(path string, name string) {
+    fileName := fmt.Sprintf("%s%s", path, name)
+    fd, err := os.Create(fileName)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    if err := fd.Close(); err != nil {
+        log.Fatal(err)
+    }
+}
+
+func genFunc(name string) {
+
+}
