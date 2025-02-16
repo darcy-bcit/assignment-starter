597f34d38d093170da29468166b452ae92234bce
evin-gg <evin4369@gmail.com>
2025-02-16 14:11:21 -0800

    added a function that adds a table with contents

--- cli/main.go
+++ cli/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gomutex/godocx"
+	"github.com/gomutex/godocx/docx"
 )
 
 const DOCUMENT_DIR string = "./output/report/"
@@ -21,7 +22,40 @@ func createDoc(docname string) {
 	}
 }
 
+func AddTableToDoc(doc *docx.RootDoc, data [][]string) {
+
+	table := doc.AddTable()
+
+	for i := 0; i < len(data); i++ {
+		row := table.AddRow()
+
+		for j := 0; j < len(data[i]); j++ {
+			row.AddCell().AddParagraph(data[i][j])
+		}
+	}
+
+}
+
 func main() {
+
+	doc, err := godocx.NewDocument()
+	if err != nil {
+		log.Fatalf("Failed to open document: %v", err)
+	}
+	data := [][]string{
+		{"ID", "Name", "Age"},
+		{"1", "Alice", "25"},
+		{"2", "Bob", "30"},
+	}
+
+	AddTableToDoc(doc, data)
+
+	err = doc.SaveTo("table.docx")
+	if err != nil {
+		log.Fatalf("Failed to save document: %v", err)
+	}
+
+	log.Println("Document saved as output.docx")
 
 	createDoc("testing")
 	createDoc("user-guide")
