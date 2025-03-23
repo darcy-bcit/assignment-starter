a018ac36958a2610a94db9ca876c25bbf20eba0e
kvnbanunu <kvnbanunu@gmail.com>
2025-03-23 04:35:58 -0700

    Add go, js, python, cpp generator

--- js/modules/codegen/index.js
+++ js/modules/codegen/index.js
@@ -1,7 +1,11 @@
 const CGenerator = require('./c-generator');
 const JavaGenerator = require('./java-generator');
-const supported = ['C', 'Java'];
-//const supported = ['C', 'Java', 'Go', 'JavaScript', 'Python', 'C++'];
+const GoGenerator = require('./go-generator');
+const JavaScriptGenerator = require('./javascript-generator');
+const PythonGenerator = require('./python-generator');
+const CPlusPlusGenerator = require('./cpp-generator');
+
+const supported = ['C', 'Java', 'Go', 'JavaScript', 'Python', 'C++'];
 
 function createGenerator(language) {
     switch (language.toLowerCase()) {
@@ -9,14 +13,14 @@ function createGenerator(language) {
             return new CGenerator();
         case 'java':
             return new JavaGenerator();
-/*        case 'go':
+        case 'go':
             return new GoGenerator();
         case 'javascript':
             return new JavaScriptGenerator();
         case 'python':
             return new PythonGenerator();
         case 'c++':
-            return new CPlusPlusGenerator(); */
+            return new CPlusPlusGenerator();
         default:
             throw new Error(`Unsupported language: ${language}. Currently supporting: ${supported.toString()}`);
     }
